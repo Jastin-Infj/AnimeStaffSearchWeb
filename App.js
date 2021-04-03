3b0e82fac960b087d2827f29335dda5cf2dd86b6
Jastin <takumiyokoo@gmail.com>
2021-04-03 17:12:33 +0900

    プラットフォームに応じてのテキスト変更 Platform.select(JSON) StyleSheet.create(JSON)

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform ,StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//プラットフォームによって変更する
+const instructions = Platform.select({
+  ios: 'Press Cmd + R',
+  android: 'Press Double Taps'
+});
+
+export default class App extends Component {
+  render(){
+    // instructionsのメッセージは プラットフォームによって変更される
+    return(
+      <View style = {styles.container}>
+        <Text style = {styles.welcome}> Hello World !</Text>
+        <Text style = {styles.instructions}> To get started , edit App.js </Text>
+        <Text style = {styles.instructions}> {instructions}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome:{
+    fontSize:20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions:{
+    textAlign:'center',
+    color:'#333333',
+    marginBottom: 5
+  }
 });
