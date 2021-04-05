6e1336445ba2cdd059b86b9fbd75093b34e70622
Jastin <takumiyokoo@gmail.com>
2021-04-05 14:34:30 +0900

    ボタンの表示処理 Button

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import {View , Image } from 'react-native';
+import {View , Button } from 'react-native';
 
 export default class App extends Component {
   render(){
     return(
-      //ローカルファイル
-      <View><Image style = {{width:64,height:64}} source = {require('./assets/favicon.png')}></Image></View>
-      //外部URL
-      //<View><Image style = {{width:64,height:64}} source = {{uri:'https://facebook.github.io/react-native/img/favicon.png'}}></Image></View>
+      //ボタン作成
+      <View style = {{flex:1, justifyContent:'center', alignContent:'center'}}>
+        <Button onPress = {() => console.warn('pressed!')} title = {'Press'}></Button>
+      </View>
     );
   }
 }
 
 ///解説
-//スタイル シート を 指定 し て い ない ので 画面 の 左上 に 表示 さ れ て い ます  
+//スタイル シート を 指定 し て い ない ので 画面 の 左上 に 表示 さ れ て い ます
