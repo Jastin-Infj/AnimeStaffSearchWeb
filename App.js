cfef856d4636854130ec340c88848d134fa23469
Jastin <takumiyokoo@gmail.com>
2021-04-05 15:10:01 +0900

    スピナー処理表示 ActivityIndicator

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import {View , Image, TouchableOpacity } from 'react-native';
+import {View , ActivityIndicator } from 'react-native';
 
 export default class App extends Component {
-  showWarning() {
-    console.warn('prwss test');
-  }
   render(){
     return(
-      //画像つき ボタン
-      <View style = {{flex:1, justifyContent:'center', alignContent:'center'}}>
-        <TouchableOpacity onPress = {this.showWarning}>
-          <Image style = {{width:64,height:64}} source = {require('./assets/favicon.png')}></Image>
-        </TouchableOpacity>
+      // animating: true スピナー処理中 false スピナー処理終了
+      // large
+      <View style = {{paddingTop:50}}>
+        <ActivityIndicator 
+          animating = {true}
+          color = '#00ff00'
+        />
       </View>
     );
-  } 
+  }
 }
-
-///解説
-//スタイル シート を 指定 し て い ない ので 画面 の 左上 に 表示 さ れ て い ます
