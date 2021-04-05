849323693ab199541aaceb1c5f1050e02579c718
Jastin <takumiyokoo@gmail.com>
2021-04-05 16:10:46 +0900

    Webページを表示させる WebView

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
-import {View , TextInput } from 'react-native';
-
-// 下線部を borderBottomWidth で実現している
+import {WebView} from 'react-native-webview';
 
 export default class App extends Component {
-  constructor(){
-    super()
-      this.state = {
-        text: ""
-      }
-  }
   render(){
     return(
-      <View style = {{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5fcff'
-      }}>
-        <TextInput
-          style = {{
-            width: '50%',
-            borderBottomWidth: 1.5,
-            borderBottomColor: '#ccc'
-          }}
-          onChangeText = {(text) =>
-            this.setState({text})}
-          value = {this.state.text}/>
-      </View>
+      <WebView
+        source = {{
+          uri:'https://amazon.co.jp/'
+        }}
+        style = {{marginTop: 20}}
+        scalesPageToFit = {true}
+      />
     );
   }
 }
