d2338191bf156a5f8554345384bb5553ee2e3713
Jastin <takumiyokoo@gmail.com>
2021-04-05 16:53:34 +0900

    日付テキスト処理(iosのみ) DateTimePicker

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import {WebView} from 'react-native-webview';
+import {View} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
+// React は 原則 state　変数を管理していく
+// DatePickerIOS(react-native) から DateTimePicker ('@react-native-community/datetimepicker') に移行
 export default class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        date: new Date()
+      }
+  }
   render(){
     return(
-      <WebView
-        source = {{
-          uri:'https://amazon.co.jp/'
-        }}
-        style = {{marginTop: 20}}
-        scalesPageToFit = {true}
-      />
+      <View style = {{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems : 'center',
+        backgroundColor: '#F5FCFF'
+      }}>
+        <DateTimePicker
+          style = {{width: 320}}
+          value = {this.state.date}
+          onDateChange = {date => {
+            this.setState({date})
+          }}
+          mode = {'date'}
+          display = {'spinner'}
+        />
+      </View>
     );
   }
 }
