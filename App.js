0084f5feb46a3ea8db53eff5e73ad37676dd2d5a
Jastin <takumiyokoo@gmail.com>
2021-04-19 07:14:21 +0900

    プラットフォーム別にTimePickerの用意 ios版の内容更新方法を記載

--- App.js
+++ App.js
@@ -1,59 +1,64 @@
-import React , {Component} from 'react';
-import {StyleSheet , View , Button , TextInput , Text, Clipboard} from 'react-native';
-//import ClipBoard from '@react-native-community/clipboard';
+import React ,  { Component } from 'react';
+import {StyleSheet, Platform, Text , View, Button , DatePickerAndroid , DatePickerIOS} from 'react-native';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+import dayjs from 'dayjs';
 
-//ClipBoardは 非推奨だが、expo では 上記の ClipBoardが利用出来なかったため 旧 ClipBoardを利用
 export default class App extends Component{
   constructor(){
-    super()
-      this.state = {
-        input: "",
-        text: ""
+    super();
+    this.state = {
+      vibrate: false,
+      date: new Date()
+    }
+  }
+
+  async openDatePickerAndroid(){
+    try{
+      const {action , year , month , day} = await DatePickerAndroid.open({
+        date: new Date(2021,4,25)
+      });
+
+      if( action !== DatePickerAndroid.dismissedAction){
+        this.setState({
+          date: new Date(year,month,day)
+        })
       }
-  }
-  pbcopy(){
-    const {input} = this.state;
-    Clipboard.setString(input);
-  }
-
-  async getpb(){
-    const text = await Clipboard.getString();
-    this.setState({text});
+    }
+    catch({code,message}){
+      console.warn('Cannot open date picker',message);
+    }
   }
 
+  //Textオブジェクトの場合は、<Text></Text>のほうがやりやすいかも
   render(){
+    const {date} = this.state;
     return(
       <View style = {{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+          backgroundColor: '#F5FCFF',
       }}>
-        <TextInput
-          style = {{
-            width: '100%',
-            textAlign: 'center',
-            borderBottomWidth: 1,
-            borderBottomColor: '#ccc'
-          }}
-            value = {this.state.input}
-            onChangeText = {
-              input =>
-                this.setState({input})
-            }/>
-        <Button
-          onPress = {() =>
-            {this.pbcopy()}
+        <Text>{`${date.getFullYear()}年　${date.getMonth() + 1}月 ${date.getDate()}日`}</Text>
+          {Platform.OS === 'ios' ? 
+            <RNDateTimePicker
+            style = {{width: 320}}
+            value = {date}
+            onChange = {(event,date) => {
+                // RNDateTimePickerの場合 {value} 属性が変更されない
+                // setState()を呼び出すことが必須になる
+                this.state.date = dayjs(date).toDate();
+                // 内容を更新してからsetStateを呼び出し
+                this.setState(this.state.date);
+              }
+            }
+            mode = {'date'}
+            display = {'spinner'}/>
+            :
+            <Button onPress = {() =>
+              this.openDatePickerAndroid()}
+              title={'日にちを選ぶ'}/>
           }
-          title = "save to ClipBoard"
-          />
-        <Text>{this.state.text}</Text>
-        <Button
-          onPress = {() =>
-            {this.getpb()}
-          }
-          title = "get from ClipBoard"
-        />
       </View>
     );
   }
