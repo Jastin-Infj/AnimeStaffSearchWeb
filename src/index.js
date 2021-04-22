782d4b84482d98e5ecf9e17ada1e10f3261e329b
Jastin <takumiyokoo@gmail.com>
2021-04-22 17:19:23 +0900

    Init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TestObject from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TestObject/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
